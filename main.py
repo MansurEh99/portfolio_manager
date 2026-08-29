6631fadd1088e26e31febf6eee89f661b17cc878
foolish_shadow <eh.mansur99@gmail.com>
2026-08-29 18:12:55 +0100

    made the buy and sell methods fro the portfolio

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
-from portfolio.portfolio import Holding, Transaction, Portfolio
+from portfolio.portfolio import Portfolio
 
 
-# Create a holding
-apple = Holding("AAPL", 10, 180)
-
-print("Holding:")
-print(apple.ticker)
-print(apple.quantity)
-print(apple.avg_cost)
-
-
-# Create a transaction
-transaction = Transaction(
-    "2026-08-29",
-    "AAPL",
-    "BUY",
-    10,
-    180
-)
-
-print("\nTransaction:")
-print(transaction.date)
-print(transaction.ticker)
-print(transaction.action)
-print(transaction.quantity)
-print(transaction.price)
-
-
-# Create a portfolio
 portfolio = Portfolio(10000)
 
-print("\nPortfolio:")
-print("Cash:", portfolio.cash)
-print("Holdings:", portfolio.holdings)
-print("Transactions:", portfolio.transactions)
+portfolio.sell(
+    "2026-08-29",
+    "AAPL",
+    5,
+    200
+)
--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -17,3 +17,34 @@ class Portfolio:
         self.cash = cash
         self.holdings = {}
         self.transactions = []
+
+    def buy(self, date: str, ticker: str, quantity: int, cost: float):
+        total_cost = cost * quantity
+        remainder = self.cash - cost
+        if remainder < 0:
+            raise ValueError("Not enough cash")
+
+        self.cash -= total_cost
+
+        if ticker in self.holdings:
+            old_cost = self.holdings[ticker].avg_cost
+            old_quantity = self.holdings[ticker].quantity
+            self.holdings[ticker].quantity += quantity
+            self.holdings[ticker].avg_cost = ((old_quantity * old_cost) + (quantity * cost)) / self.holdings[ticker].quantity
+        else:
+            self.holdings[ticker] = Holding(ticker, quantity, cost)
+
+        self.transactions.append(Transaction(date, ticker, "BUY", quantity, cost))
+
+    def sell(self, date: str, ticker: str, quantity: float, price: int):
+        if ticker not in self.holdings or self.holdings[ticker].quantity >= quantity:
+            raise ValueError(f"you dont have enough {ticker} to complete operation")
+
+        proceeds = quantity * price
+        self.cash += proceeds
+        self.holdings[ticker].quantity -= quantity
+
+        if self.holdings[ticker].quantity == 0:
+            del self.holdings[ticker]
+
+        self.transactions.append(Transaction(date, ticker, "SELL", quantity, price))
